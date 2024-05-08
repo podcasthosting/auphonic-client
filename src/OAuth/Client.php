cda92c3dc1bca1b31fcc4f129f2a5455f8a5534a
Fabio <info1@open-haus.de>
2024-05-08 10:48:26 +0200

    OAuth2 client to authenticate against Auphonic API added

---
+++ src/OAuth/Client.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * User: fabio
+ * Date: 08.05.24
+ * Time: 10:35
+ */
+
+namespace podcasthosting\Auphonic\OAuth;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use Psr\Http\Message\ResponseInterface;
+
+class Client extends AbstractProvider
+{
+    const AUPHONIC_URL = 'https://auphonic.com/';
+
+    /**
+     * @inheritDoc
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return self::AUPHONIC_URL . '/oauth2/authorize/';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return self::AUPHONIC_URL . '/oauth2/token/';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return self::AUPHONIC_URL . '/api/user.json?bearer_token=' . $token->getToken();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getDefaultScopes()
+    {
+        return ['*'];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if ($response->getStatusCode() >= 400) {
+            throw new IdentityProviderException('Invalid response from Auphonic', $response->getStatusCode(), $response->getBody()->getContents());
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        throw new \Exception('Not implemented.');
+    }
+}
